3abbbda525cb81ca1f8b9c19f328935c92047558
garyleung142857 <garyleung142857@gmail.com>
2023-08-01 10:40:55 +0800

    pwa

---
+++ precache-manifest.57060ead89c82fab93384dda2aa40154.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e748b707c37c028a70",
+    "url": "/bidding-box/css/app.9dadecf4.css"
+  },
+  {
+    "revision": "b581304a3002e769da7c",
+    "url": "/bidding-box/css/chunk-vendors.1949bf3e.css"
+  },
+  {
+    "revision": "62fcb1f956ea3db28a8b0bef950e1cad",
+    "url": "/bidding-box/index.html"
+  },
+  {
+    "revision": "d7e748b707c37c028a70",
+    "url": "/bidding-box/js/app.455b0dac.js"
+  },
+  {
+    "revision": "b581304a3002e769da7c",
+    "url": "/bidding-box/js/chunk-vendors.e764a917.js"
+  },
+  {
+    "revision": "ab49545e4f20da0f4a9298a033ae05e2",
+    "url": "/bidding-box/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bidding-box/robots.txt"
+  }
+]);
