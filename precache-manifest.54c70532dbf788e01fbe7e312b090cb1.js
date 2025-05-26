b1d4069615b63ec22dc68c7f1002082b328316ac
garyleung142857 <garyleung142857@gmail.com>
2025-05-26 15:26:31 +0800

    Updates

---
+++ precache-manifest.54c70532dbf788e01fbe7e312b090cb1.js
@@ -0,0 +1,474 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f720c4a0122ba762b57",
+    "url": "/bidding-box/css/app.9f7221a8.css"
+  },
+  {
+    "revision": "a72de398aa82d1974102",
+    "url": "/bidding-box/css/chunk-vendors.5985762e.css"
+  },
+  {
+    "revision": "026b7ac9c43c7e04250f00acd510fa49",
+    "url": "/bidding-box/fonts/materialdesignicons-webfont.026b7ac9.woff"
+  },
+  {
+    "revision": "1d7bcee1b302339c3b8db10214dc9ec6",
+    "url": "/bidding-box/fonts/materialdesignicons-webfont.1d7bcee1.woff2"
+  },
+  {
+    "revision": "6e435534bd35da5fef04168860a9b8fa",
+    "url": "/bidding-box/fonts/materialdesignicons-webfont.6e435534.ttf"
+  },
+  {
+    "revision": "8ced95a05e3c82d7b6ef6cd3f31c7ccb",
+    "url": "/bidding-box/fonts/materialdesignicons-webfont.8ced95a0.eot"
+  },
+  {
+    "revision": "09c5f3023512e996696f61449b62662a",
+    "url": "/bidding-box/fonts/noto-sans-hk-0-wght-normal.09c5f302.woff2"
+  },
+  {
+    "revision": "71b3082fe04ad43ec4168e30947c07db",
+    "url": "/bidding-box/fonts/noto-sans-hk-1-wght-normal.71b3082f.woff2"
+  },
+  {
+    "revision": "3a4cfb88ecdbe4c76247009ce43a92d4",
+    "url": "/bidding-box/fonts/noto-sans-hk-10-wght-normal.3a4cfb88.woff2"
+  },
+  {
+    "revision": "1b8884620b35ee3d36bafa67157923b8",
+    "url": "/bidding-box/fonts/noto-sans-hk-100-wght-normal.1b888462.woff2"
+  },
+  {
+    "revision": "8a6e5ef61af8e8ff9162662b4e9859b7",
+    "url": "/bidding-box/fonts/noto-sans-hk-101-wght-normal.8a6e5ef6.woff2"
+  },
+  {
+    "revision": "d3f0778963792eb8fd9da5b07082472c",
+    "url": "/bidding-box/fonts/noto-sans-hk-102-wght-normal.d3f07789.woff2"
+  },
+  {
+    "revision": "ab89950c03fbb7ff3572de5c6326184f",
+    "url": "/bidding-box/fonts/noto-sans-hk-103-wght-normal.ab89950c.woff2"
+  },
+  {
+    "revision": "01f2df90c18e03764c79b3fa0bdc85c1",
+    "url": "/bidding-box/fonts/noto-sans-hk-104-wght-normal.01f2df90.woff2"
+  },
+  {
+    "revision": "1b1d99cb67c8d266da883bbfdab1a8b8",
+    "url": "/bidding-box/fonts/noto-sans-hk-105-wght-normal.1b1d99cb.woff2"
+  },
+  {
+    "revision": "c04a2fb0a5b27733ec117a5b976fd4ca",
+    "url": "/bidding-box/fonts/noto-sans-hk-106-wght-normal.c04a2fb0.woff2"
+  },
+  {
+    "revision": "abd1242f07888461c0717d1b63cd7d24",
+    "url": "/bidding-box/fonts/noto-sans-hk-107-wght-normal.abd1242f.woff2"
+  },
+  {
+    "revision": "1e9e770e3445bae4692febaaf24fb485",
+    "url": "/bidding-box/fonts/noto-sans-hk-108-wght-normal.1e9e770e.woff2"
+  },
+  {
+    "revision": "63795a838345ba95c35d4530c60df7ce",
+    "url": "/bidding-box/fonts/noto-sans-hk-109-wght-normal.63795a83.woff2"
+  },
+  {
+    "revision": "e2d8d7f12424a525385d31a0aaee9e29",
+    "url": "/bidding-box/fonts/noto-sans-hk-110-wght-normal.e2d8d7f1.woff2"
+  },
+  {
+    "revision": "551af2f76e8ba5e31e0b56da450206ea",
+    "url": "/bidding-box/fonts/noto-sans-hk-111-wght-normal.551af2f7.woff2"
+  },
+  {
+    "revision": "5007e8806b2a641f35ffac11b13ef6d3",
+    "url": "/bidding-box/fonts/noto-sans-hk-112-wght-normal.5007e880.woff2"
+  },
+  {
+    "revision": "d769eed0406cee481e07fbc7a4fb316f",
+    "url": "/bidding-box/fonts/noto-sans-hk-113-wght-normal.d769eed0.woff2"
+  },
+  {
+    "revision": "e456a1fafd1985f0b00e0d69487201b1",
+    "url": "/bidding-box/fonts/noto-sans-hk-114-wght-normal.e456a1fa.woff2"
+  },
+  {
+    "revision": "ff9e7684a74040ce425c37b1bd205080",
+    "url": "/bidding-box/fonts/noto-sans-hk-115-wght-normal.ff9e7684.woff2"
+  },
+  {
+    "revision": "ceaf8cc7946e0a06763baa0c399552f3",
+    "url": "/bidding-box/fonts/noto-sans-hk-116-wght-normal.ceaf8cc7.woff2"
+  },
+  {
+    "revision": "cc36079b1ebcbd879ccb2adf91a5ec21",
+    "url": "/bidding-box/fonts/noto-sans-hk-117-wght-normal.cc36079b.woff2"
+  },
+  {
+    "revision": "e97d37946ab577e92b755e62e7834d56",
+    "url": "/bidding-box/fonts/noto-sans-hk-118-wght-normal.e97d3794.woff2"
+  },
+  {
+    "revision": "0747c11272a6c077eb85a5392fbb507f",
+    "url": "/bidding-box/fonts/noto-sans-hk-119-wght-normal.0747c112.woff2"
+  },
+  {
+    "revision": "e0bb2bb9d85b6b219e22cf59e37edec4",
+    "url": "/bidding-box/fonts/noto-sans-hk-15-wght-normal.e0bb2bb9.woff2"
+  },
+  {
+    "revision": "355715c64d00fbc6c5049805a372555d",
+    "url": "/bidding-box/fonts/noto-sans-hk-16-wght-normal.355715c6.woff2"
+  },
+  {
+    "revision": "f3e3ec9e66a99846c126c410ed032727",
+    "url": "/bidding-box/fonts/noto-sans-hk-2-wght-normal.f3e3ec9e.woff2"
+  },
+  {
+    "revision": "8fd2b061466a6ee0682c97772e1d118f",
+    "url": "/bidding-box/fonts/noto-sans-hk-25-wght-normal.8fd2b061.woff2"
+  },
+  {
+    "revision": "822d780490efe60ef743bbd881b31242",
+    "url": "/bidding-box/fonts/noto-sans-hk-26-wght-normal.822d7804.woff2"
+  },
+  {
+    "revision": "29b8af084b7de15ba8b9afb731673894",
+    "url": "/bidding-box/fonts/noto-sans-hk-27-wght-normal.29b8af08.woff2"
+  },
+  {
+    "revision": "73c183bcd31c772487612ca4f0b57476",
+    "url": "/bidding-box/fonts/noto-sans-hk-28-wght-normal.73c183bc.woff2"
+  },
+  {
+    "revision": "d0a0b37d0beeba8125ed2cce3cb75315",
+    "url": "/bidding-box/fonts/noto-sans-hk-29-wght-normal.d0a0b37d.woff2"
+  },
+  {
+    "revision": "37a144e83dc1c7b044bd7ac9ce4a7c36",
+    "url": "/bidding-box/fonts/noto-sans-hk-3-wght-normal.37a144e8.woff2"
+  },
+  {
+    "revision": "06a04f431518899f0700eb3a5da76db6",
+    "url": "/bidding-box/fonts/noto-sans-hk-30-wght-normal.06a04f43.woff2"
+  },
+  {
+    "revision": "bbfdf0ae181a77bcb33603f648473ae0",
+    "url": "/bidding-box/fonts/noto-sans-hk-31-wght-normal.bbfdf0ae.woff2"
+  },
+  {
+    "revision": "015e0ff43d850622dd38c0c35c54264e",
+    "url": "/bidding-box/fonts/noto-sans-hk-32-wght-normal.015e0ff4.woff2"
+  },
+  {
+    "revision": "4a97d3a7c1416120d36ecb3921870b39",
+    "url": "/bidding-box/fonts/noto-sans-hk-33-wght-normal.4a97d3a7.woff2"
+  },
+  {
+    "revision": "aa21ba31210e9f2ced3324eaf9109168",
+    "url": "/bidding-box/fonts/noto-sans-hk-34-wght-normal.aa21ba31.woff2"
+  },
+  {
+    "revision": "9aa6edec508a93616fddb986970da19c",
+    "url": "/bidding-box/fonts/noto-sans-hk-35-wght-normal.9aa6edec.woff2"
+  },
+  {
+    "revision": "b2a7a3552ebc9b19ce29a0533e5b3a63",
+    "url": "/bidding-box/fonts/noto-sans-hk-36-wght-normal.b2a7a355.woff2"
+  },
+  {
+    "revision": "7a5d8fe879f3c00ad6071fb183701620",
+    "url": "/bidding-box/fonts/noto-sans-hk-37-wght-normal.7a5d8fe8.woff2"
+  },
+  {
+    "revision": "c41e4dbcc93c5ee64404e990a44646ac",
+    "url": "/bidding-box/fonts/noto-sans-hk-38-wght-normal.c41e4dbc.woff2"
+  },
+  {
+    "revision": "f7c220d78fc8c983b2fb6897aba85b7c",
+    "url": "/bidding-box/fonts/noto-sans-hk-39-wght-normal.f7c220d7.woff2"
+  },
+  {
+    "revision": "352a15c426396988d5941fc7340ac664",
+    "url": "/bidding-box/fonts/noto-sans-hk-4-wght-normal.352a15c4.woff2"
+  },
+  {
+    "revision": "700bedaccaccea6415ce966082cbe18c",
+    "url": "/bidding-box/fonts/noto-sans-hk-40-wght-normal.700bedac.woff2"
+  },
+  {
+    "revision": "bc6b2fe4122c919eedd3dab6b1f386bc",
+    "url": "/bidding-box/fonts/noto-sans-hk-41-wght-normal.bc6b2fe4.woff2"
+  },
+  {
+    "revision": "421573ba935538bc750d1556110cbc32",
+    "url": "/bidding-box/fonts/noto-sans-hk-42-wght-normal.421573ba.woff2"
+  },
+  {
+    "revision": "603d3489cb7bf925f75d3be3041b3933",
+    "url": "/bidding-box/fonts/noto-sans-hk-43-wght-normal.603d3489.woff2"
+  },
+  {
+    "revision": "0184c333da09915db3b868dd68cb6c3d",
+    "url": "/bidding-box/fonts/noto-sans-hk-44-wght-normal.0184c333.woff2"
+  },
+  {
+    "revision": "5de2e86aa1babac2cc365cad16a91c01",
+    "url": "/bidding-box/fonts/noto-sans-hk-45-wght-normal.5de2e86a.woff2"
+  },
+  {
+    "revision": "cf186373c89029049c424cb218fcd150",
+    "url": "/bidding-box/fonts/noto-sans-hk-46-wght-normal.cf186373.woff2"
+  },
+  {
+    "revision": "c1ad7ba7f827d00c304d2ff6eb245323",
+    "url": "/bidding-box/fonts/noto-sans-hk-47-wght-normal.c1ad7ba7.woff2"
+  },
+  {
+    "revision": "f87e1fc7ffe0b2ffdc3fa706f06cbf86",
+    "url": "/bidding-box/fonts/noto-sans-hk-48-wght-normal.f87e1fc7.woff2"
+  },
+  {
+    "revision": "6db09bb0dbc386287735360fbe913f15",
+    "url": "/bidding-box/fonts/noto-sans-hk-49-wght-normal.6db09bb0.woff2"
+  },
+  {
+    "revision": "bcd1250ceeede6a9e906528ccef3bd26",
+    "url": "/bidding-box/fonts/noto-sans-hk-5-wght-normal.bcd1250c.woff2"
+  },
+  {
+    "revision": "aa23baaed0ac8ea68cfeee0fe487088a",
+    "url": "/bidding-box/fonts/noto-sans-hk-50-wght-normal.aa23baae.woff2"
+  },
+  {
+    "revision": "697457ffa72d529a6e09375b2ee13bba",
+    "url": "/bidding-box/fonts/noto-sans-hk-51-wght-normal.697457ff.woff2"
+  },
+  {
+    "revision": "62e0c110c47254b505353298385ed31a",
+    "url": "/bidding-box/fonts/noto-sans-hk-52-wght-normal.62e0c110.woff2"
+  },
+  {
+    "revision": "a7d2b77368fde19ae429783806a7aa2a",
+    "url": "/bidding-box/fonts/noto-sans-hk-53-wght-normal.a7d2b773.woff2"
+  },
+  {
+    "revision": "720d143065f58e8177a2d068caa6e537",
+    "url": "/bidding-box/fonts/noto-sans-hk-54-wght-normal.720d1430.woff2"
+  },
+  {
+    "revision": "2620a1d9f5116925e2f4cd6bfc13884c",
+    "url": "/bidding-box/fonts/noto-sans-hk-55-wght-normal.2620a1d9.woff2"
+  },
+  {
+    "revision": "6859321c6f5b51d6c4b00f19c9e62451",
+    "url": "/bidding-box/fonts/noto-sans-hk-56-wght-normal.6859321c.woff2"
+  },
+  {
+    "revision": "f78ee599e6ed893b66fca7bca13a0129",
+    "url": "/bidding-box/fonts/noto-sans-hk-57-wght-normal.f78ee599.woff2"
+  },
+  {
+    "revision": "f86f063a83a3ae408ecee77fcd4f1e8b",
+    "url": "/bidding-box/fonts/noto-sans-hk-58-wght-normal.f86f063a.woff2"
+  },
+  {
+    "revision": "856b4b92855ed4b13a6169f275e110e8",
+    "url": "/bidding-box/fonts/noto-sans-hk-59-wght-normal.856b4b92.woff2"
+  },
+  {
+    "revision": "f0e57b42732ef5339f376b726ddad50a",
+    "url": "/bidding-box/fonts/noto-sans-hk-6-wght-normal.f0e57b42.woff2"
+  },
+  {
+    "revision": "6ec53cfd25b463187eb83280344d184c",
+    "url": "/bidding-box/fonts/noto-sans-hk-60-wght-normal.6ec53cfd.woff2"
+  },
+  {
+    "revision": "68243e869d5407a63be4806e1bb05105",
+    "url": "/bidding-box/fonts/noto-sans-hk-61-wght-normal.68243e86.woff2"
+  },
+  {
+    "revision": "d0718b1e595dfcfff0c0684f565147e5",
+    "url": "/bidding-box/fonts/noto-sans-hk-62-wght-normal.d0718b1e.woff2"
+  },
+  {
+    "revision": "dcfa268e8ee19ce2d21e74627d1093ba",
+    "url": "/bidding-box/fonts/noto-sans-hk-63-wght-normal.dcfa268e.woff2"
+  },
+  {
+    "revision": "e07cbce5adf3ef6b7f0a25503f952994",
+    "url": "/bidding-box/fonts/noto-sans-hk-64-wght-normal.e07cbce5.woff2"
+  },
+  {
+    "revision": "b6b9ecb413dc960d700190f99abbcb90",
+    "url": "/bidding-box/fonts/noto-sans-hk-65-wght-normal.b6b9ecb4.woff2"
+  },
+  {
+    "revision": "7e14cd0a0078a0fe8a0a477961482c84",
+    "url": "/bidding-box/fonts/noto-sans-hk-66-wght-normal.7e14cd0a.woff2"
+  },
+  {
+    "revision": "c55029d59e90526f303bc7570aebb6fe",
+    "url": "/bidding-box/fonts/noto-sans-hk-67-wght-normal.c55029d5.woff2"
+  },
+  {
+    "revision": "53e559461441914e66f6d2f4ab41f242",
+    "url": "/bidding-box/fonts/noto-sans-hk-68-wght-normal.53e55946.woff2"
+  },
+  {
+    "revision": "daff42a1e4f8dc88701c82bb7b2f1246",
+    "url": "/bidding-box/fonts/noto-sans-hk-69-wght-normal.daff42a1.woff2"
+  },
+  {
+    "revision": "8db29699d18b5d1eeb05a12ea5093007",
+    "url": "/bidding-box/fonts/noto-sans-hk-7-wght-normal.8db29699.woff2"
+  },
+  {
+    "revision": "ac43b56f32a23f53084b7ea31573bf37",
+    "url": "/bidding-box/fonts/noto-sans-hk-70-wght-normal.ac43b56f.woff2"
+  },
+  {
+    "revision": "02b67fbc1a5bc31ddaaec84866ad8463",
+    "url": "/bidding-box/fonts/noto-sans-hk-71-wght-normal.02b67fbc.woff2"
+  },
+  {
+    "revision": "4b55ce32fcb03b3eaa0e83e860522fa5",
+    "url": "/bidding-box/fonts/noto-sans-hk-72-wght-normal.4b55ce32.woff2"
+  },
+  {
+    "revision": "3b4a2f1cbb8bd76593a7e393f2016428",
+    "url": "/bidding-box/fonts/noto-sans-hk-73-wght-normal.3b4a2f1c.woff2"
+  },
+  {
+    "revision": "5198df8833b53a049839b4f9e287dad4",
+    "url": "/bidding-box/fonts/noto-sans-hk-74-wght-normal.5198df88.woff2"
+  },
+  {
+    "revision": "f74393257b98fd5a1c554b183f26eb0b",
+    "url": "/bidding-box/fonts/noto-sans-hk-75-wght-normal.f7439325.woff2"
+  },
+  {
+    "revision": "2b72e5080f2f637b0051bfc16f074d00",
+    "url": "/bidding-box/fonts/noto-sans-hk-76-wght-normal.2b72e508.woff2"
+  },
+  {
+    "revision": "2377a03c5c255a641bdbbc40e52d0cab",
+    "url": "/bidding-box/fonts/noto-sans-hk-77-wght-normal.2377a03c.woff2"
+  },
+  {
+    "revision": "35dcc62d4b7092b349c12579bfc72449",
+    "url": "/bidding-box/fonts/noto-sans-hk-78-wght-normal.35dcc62d.woff2"
+  },
+  {
+    "revision": "c3d3dd5296935a10b8d93810d7680891",
+    "url": "/bidding-box/fonts/noto-sans-hk-79-wght-normal.c3d3dd52.woff2"
+  },
+  {
+    "revision": "0147c74ebd73216e145fa0a418f15cec",
+    "url": "/bidding-box/fonts/noto-sans-hk-8-wght-normal.0147c74e.woff2"
+  },
+  {
+    "revision": "7429744b654a301e8b552db13896bea7",
+    "url": "/bidding-box/fonts/noto-sans-hk-80-wght-normal.7429744b.woff2"
+  },
+  {
+    "revision": "ea61b4928462635a7fbd51f581b081ba",
+    "url": "/bidding-box/fonts/noto-sans-hk-81-wght-normal.ea61b492.woff2"
+  },
+  {
+    "revision": "92a68b6cfa20143df22c9076f7b9b170",
+    "url": "/bidding-box/fonts/noto-sans-hk-82-wght-normal.92a68b6c.woff2"
+  },
+  {
+    "revision": "90d0e6ca954243f70dd8f7a041bd9bfc",
+    "url": "/bidding-box/fonts/noto-sans-hk-83-wght-normal.90d0e6ca.woff2"
+  },
+  {
+    "revision": "152b5eae32dc00c682117d020e5a3cee",
+    "url": "/bidding-box/fonts/noto-sans-hk-84-wght-normal.152b5eae.woff2"
+  },
+  {
+    "revision": "dc40f8ff09f6652ffaea6f8cd6646d3e",
+    "url": "/bidding-box/fonts/noto-sans-hk-85-wght-normal.dc40f8ff.woff2"
+  },
+  {
+    "revision": "0d8d9be40d8659d0fa53cefedf0e9fc8",
+    "url": "/bidding-box/fonts/noto-sans-hk-86-wght-normal.0d8d9be4.woff2"
+  },
+  {
+    "revision": "6836f272c2ae30efdea6e78a70dbb4fb",
+    "url": "/bidding-box/fonts/noto-sans-hk-87-wght-normal.6836f272.woff2"
+  },
+  {
+    "revision": "fcbfe7c4407bc43dcbe967679aa6c3ff",
+    "url": "/bidding-box/fonts/noto-sans-hk-88-wght-normal.fcbfe7c4.woff2"
+  },
+  {
+    "revision": "3c67602a6b6c58d3cce4531caffb3be1",
+    "url": "/bidding-box/fonts/noto-sans-hk-89-wght-normal.3c67602a.woff2"
+  },
+  {
+    "revision": "bd4be54e49dc4c8a1274ca5ac5033f4b",
+    "url": "/bidding-box/fonts/noto-sans-hk-9-wght-normal.bd4be54e.woff2"
+  },
+  {
+    "revision": "f672a38f1b07f0739979bfdffe8b2520",
+    "url": "/bidding-box/fonts/noto-sans-hk-90-wght-normal.f672a38f.woff2"
+  },
+  {
+    "revision": "64f8a9b134d0238d04f2a0384fa52213",
+    "url": "/bidding-box/fonts/noto-sans-hk-91-wght-normal.64f8a9b1.woff2"
+  },
+  {
+    "revision": "c508d892c1bc7b65e8ff943a1cf31eee",
+    "url": "/bidding-box/fonts/noto-sans-hk-92-wght-normal.c508d892.woff2"
+  },
+  {
+    "revision": "75790c7770113094793388e1dd8508ac",
+    "url": "/bidding-box/fonts/noto-sans-hk-93-wght-normal.75790c77.woff2"
+  },
+  {
+    "revision": "99e709e6d32bbe02816456bcdb289869",
+    "url": "/bidding-box/fonts/noto-sans-hk-99-wght-normal.99e709e6.woff2"
+  },
+  {
+    "revision": "70ee681e10f4f745d3d1d1c3f848fb6e",
+    "url": "/bidding-box/fonts/noto-sans-hk-cyrillic-wght-normal.70ee681e.woff2"
+  },
+  {
+    "revision": "8398aa660af90726c15116084b58b74d",
+    "url": "/bidding-box/fonts/noto-sans-hk-latin-ext-wght-normal.8398aa66.woff2"
+  },
+  {
+    "revision": "0d793d9f3b2e698c1a0e7d7067378c75",
+    "url": "/bidding-box/fonts/noto-sans-hk-latin-wght-normal.0d793d9f.woff2"
+  },
+  {
+    "revision": "c07f75d3bb85ebfb0dac528d4735c1ba",
+    "url": "/bidding-box/fonts/noto-sans-hk-vietnamese-wght-normal.c07f75d3.woff2"
+  },
+  {
+    "revision": "c7761b89b1607103a59ebca3ad106eda",
+    "url": "/bidding-box/index.html"
+  },
+  {
+    "revision": "9f720c4a0122ba762b57",
+    "url": "/bidding-box/js/app.70257355.js"
+  },
+  {
+    "revision": "a72de398aa82d1974102",
+    "url": "/bidding-box/js/chunk-vendors.de762690.js"
+  },
+  {
+    "revision": "ab49545e4f20da0f4a9298a033ae05e2",
+    "url": "/bidding-box/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bidding-box/robots.txt"
+  }
+]);
